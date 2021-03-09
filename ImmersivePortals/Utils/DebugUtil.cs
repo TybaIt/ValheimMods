9e3dc6b5e1efbe5eea115fd99f5f91bec0465fa7
Andreas G <andreas.gaertner93@gmail.com>
2021-03-09 13:26:08 +0100

    :zap: Update to use string.Format

--- ImmersivePortals/Utils/DebugUtil.cs
+++ ImmersivePortals/Utils/DebugUtil.cs
@@ -4,22 +4,20 @@ namespace ImmersivePortals
 {
     public static class DebugUtil
     {
-        private static void Log(string message, LogLevel level = LogLevel.None, params string[] args)
+        private static void Log(string message, LogLevel level = LogLevel.None, params object[] args)
         {
-            for (int i = 0; i < args.Length; i++) {
-                message = message.Replace($"{{{i}}}", args[i]);
-            }
+            message = string.Format(message, args);
             ImmersivePortals.context.log.Log(level, message);
             if (ImmersivePortals.enableNotifications.Value) {
                 Player.m_localPlayer.Message(MessageHud.MessageType.TopLeft, message);
             }
         }
-        public static void Log(string message, params string[] args) => Log(message, LogLevel.None, args);
-        public static void LogWarning(string message, params string[] args) => Log(message, LogLevel.Warning, args);
-        public static void LogDebug(string message, params string[] args) => Log(message, LogLevel.Debug, args);
-        public static void LogError(string message, params string[] args) => Log(message, LogLevel.Error, args);
-        public static void LogInfo(string message, params string[] args) => Log(message, LogLevel.Info, args);
-        public static void LogMessage(string message, params string[] args) => Log(message, LogLevel.Message, args);
-        public static void LogFatal(string message, params string[] args) => Log(message, LogLevel.Fatal, args);
+        public static void Log(string message, params object[] args) => Log(message, LogLevel.None, args);
+        public static void LogWarning(string message, params object[] args) => Log(message, LogLevel.Warning, args);
+        public static void LogDebug(string message, params object[] args) => Log(message, LogLevel.Debug, args);
+        public static void LogError(string message, params object[] args) => Log(message, LogLevel.Error, args);
+        public static void LogInfo(string message, params object[] args) => Log(message, LogLevel.Info, args);
+        public static void LogMessage(string message, params object[] args) => Log(message, LogLevel.Message, args);
+        public static void LogFatal(string message, params object[] args) => Log(message, LogLevel.Fatal, args);
     }
 }
